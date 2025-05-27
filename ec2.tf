416ce49709cd0be7fa4bf0dfa456aa03c4e72631
balaguru-20 <balaguravaiah0606@gmail.com>
2025-05-28 01:06:01 +0530

    terraform for docker ec2 instance

---
+++ ec2.tf
@@ -0,0 +1,52 @@
+resource "aws_instance" "this" {
+  ami                    = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "t3.micro"
+
+  #20gb is not enough
+  root_block_device {
+    volume_size = 50    #Set root volume size to 50gb
+    volume_type = "gp3" # Use gp3 for better performance (optional)
+  }
+  user_data = file("user-data.sh")
+  tags = {
+    Name = "docker"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound and all outbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
